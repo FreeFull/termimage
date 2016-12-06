4ed371cd56d13fb4db3b830cacef452fd2d3180d
Filip Szczepański <jazz2rulez@gmail.com>
2016-12-06 23:34:25 +0000

    Add documentation and simplify code somewhat.

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,27 @@ use image::GenericImage;
 fn main() {
     if let Some(filename) = env::args().nth(1) {
         let image = image::open(&filename).unwrap();
-        let (_, height) = image.dimensions();
+        let (width, height) = image.dimensions();
+        // Each row of characters in the terminal corresponds to two rows in the image.
+        // A half-block character is used to represent two pixels, with the foreground
+        // colour being that of the top pixel, and the background colour being the
+        // bottom pixel.
         for y in 0..((height + 1) / 2) {
-            for x in 0..image.width() {
+            for x in 0..width {
                 let data = image.get_pixel(x, y*2).data;
-                let top_pixel = Fg(Rgb(data[0], data[1], data[2]));
-                let bottom_pixel;
+                // Set top pixel colour
+                print!("{}", Fg(Rgb(data[0], data[1], data[2])));
+                // If the image has an odd height, the last row of pixels
                 if image.in_bounds(x, y*2 + 1) {
                     let data = image.get_pixel(x, y*2 + 1).data;
-                    bottom_pixel = Bg(Rgb(data[0], data[1], data[2]));
+                    // Set bottom pixel colour
+                    print!("{}", Bg(Rgb(data[0], data[1], data[2])));
                 } else {
-                    bottom_pixel = Bg(Rgb(0,0,0));
+                    // Leave the bottom at the default colour.
+                    // In terminals with a transparent background, this will be transparent as well.
+                    print!("{}", Bg(Reset));
                 }
-                print!("{}{}▀", top_pixel, bottom_pixel);
+                print!("▀");
             }
             println!("{}{}", Fg(Reset), Bg(Reset));
         }
